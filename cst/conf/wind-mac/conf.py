dcfdd1bad19ac9b2724a66f69ffabdb1e0dd9139
fengw <fengw@feng.usc.edu>
2010-10-26 16:25:41 -0700

    add wind-mac conf

---
+++ cst/conf/wind-mac/conf.py
@@ -0,0 +1,13 @@
+"""
+wind-mac
+Intel(R) Core 2 Duo 2.0 GHz
+2 GB 
+"""
+login = hostname = 'iMac-wind.local'
+login = hostname = 'feng.usc.edu'
+maxcores = 2
+maxram = 800
+rate = 1.0e6
+mode = 'm'
+fortran_serial = 'gfortran'
+
