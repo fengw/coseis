18576d54da5f5dd4db1a351550535f87a19d2eed
fengw <fengw@iMac-wind.local>
2010-11-03 00:53:23 -0700

    update from Gely

---
+++ cst/conf/wind-mac/conf.py
@@ -0,0 +1,14 @@
+"""
+wind-mac configure file for coseis and sord install on iMac.wind
+Intel(R) Core 2 Duo 2.0 GHz
+2 GB 
+"""
+login = hostname = 'feng.usc.edu'
+login = hostname = 'iMac-wind.local'
+repo = '/Users/fengw/local/coseis/data'
+maxcores = 2
+maxram = 800
+rate = 1.0e6
+mode = 'm'
+fortran_serial = 'gfortran'
+
