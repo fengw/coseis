3ba840a1f6b0a5898a60f67695d7621117f360ce
fengw <fengw@usc.edu>
2010-11-19 14:26:17 -0800

    new coseis

--- util/archive.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-
-cd "$( dirname $( dirname $0 ) )"
-git archive --prefix=coseis/ -o coseis.tar HEAD
-git log -100 > changelog
-tar -s,^,coseis/, -rf coseis.tar changelog
-gzip -c coseis.tar > download/coseis.tgz
-rm changelog coseis.tar
-
